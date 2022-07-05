1a14ec233d5a05dd9ec6e920b324f3a7a0da9308
HuzefaTariq <101399969+huzefaTariq1@users.noreply.github.com>
2022-07-05 22:17:27 +0500

    feature delete data fromStore
    data delete from fire store

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
+import { projectFirestore } from '../firebase/config';
 const RecipeList = ({recipe}) => {
   let navigate = useNavigate();
+ 
+  const handleDelete=(id)=>{
+     projectFirestore.collection('recipes').doc(id).delete()
+  }
 
   return (
     <>
@@ -12,6 +17,7 @@ const RecipeList = ({recipe}) => {
         <h1 className='text-gray-500 text-xl'>{recipe.cookingTime}</h1>
         <h1 className='text-gray-500 text-sm ' >{recipe.method.substring(0, 100)} </h1>
         <center><button onClick={()=>navigate(`/recipes/${recipe.id}`)} className='bg-gray-300 p-1 px-4 rounded-lg mt-3'>Cook This</button></center>
+        <button onClick={()=>handleDelete(recipe.id)}>delete</button>
         </div>
        </div>
 
